c681463a57ffd4293ed77c384188366ede0f1da1
Javier Alavrez <javieralvarez82@live.com>
2019-10-22 22:52:26 -0700

    working on searching list of friends

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,4 +1,4 @@
-
+import random
 
 class User:
     def __init__(self, name):
@@ -29,7 +29,7 @@ class SocialGraph:
         self.lastID += 1  # automatically increment the ID to assign the new user
         self.users[self.lastID] = User(name)
         self.friendships[self.lastID] = set()
-
+    
     def populateGraph(self, numUsers, avgFriendships):
         """
         Takes a number of users and an average number of friendships
@@ -47,13 +47,34 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
-
+        for user in range(0, numUsers):
+            self.addUser(user)
         # Create friendships
+        totalFriendships = numUsers * avgFriendships
+        times_to_call_addFriendship = totalFriendships // 2
+        userIds = range(1, numUsers + 1)
+        
+        print("Times Friends", times_to_call_addFriendship)
+        
+        friends = []
+        for user in userIds:
+            for friend in range(user + 1 , numUsers + 1):
+                friends.append((user, friend))
+
+        friends_to_make = friends[0:times_to_call_addFriendship]
+        #shuffle the list
+        random.shuffle(friends)
+        print("FRIENDS", friends)
+        # (10, 2) ----> friends has 45 possible friendships
+        #but we want to call self.addFriendship 10 times, to make
+        #20 total friendships so we only take 10 friendship combos
+        #from the front of our list
+        for friendship in friends_to_make:
+            self.addFriendship(friendship[0], friendship[1])
 
     def getAllSocialPaths(self, userID):
         """
         Takes a user's userID as an argument
-
         Returns a dictionary containing every user in that user's
         extended network with the shortest friendship path between them.
 
@@ -61,8 +82,10 @@ class SocialGraph:
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+        #look for userId and a list of friends
+        if userID in self.friendships:
+            for idx in range
         return visited
-
 
 if __name__ == '__main__':
     sg = SocialGraph()
